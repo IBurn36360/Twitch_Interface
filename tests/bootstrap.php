5cd43254c2177d98258559fd3750c91d3f91b02a
Anthony Diaz <iburn36360@gmail.com>
2017-03-11 20:00:00 -0700

    Moving environment to Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,6 @@
 
 date_default_timezone_set('UTC');
 
-// Run the composer autoload
-require_once(__DIR__ . '/../vendor/autoload.php');
-
 // Set our constants to the global env for travis if env exists
 if (getenv('TWITCH_TEST_CLIENT_ID') !== false) {
     define('TWITCH_TEST_CLIENT_ID', getenv('TWITCH_TEST_CLIENT_ID'));
@@ -13,3 +10,6 @@ if (getenv('TWITCH_TEST_CLIENT_ID') !== false) {
 if (getenv('TWITCH_TEST_CLIENT_SECRET') !== false) {
     define('TWITCH_TEST_CLIENT_SECRET', getenv('TWITCH_TEST_CLIENT_SECRET'));
 }
+
+// Run the composer autoload
+require_once(__DIR__ . '/../vendor/autoload.php');
