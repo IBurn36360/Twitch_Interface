3cfe204ee64bbbbb5c3e2e8c71cf94ca6b666276
Anthony Diaz <iburn36360@gmail.com>
2017-03-14 09:12:19 -0600

    FIX: Removing env checks for travis was not a good idea
    This broke the travis test set

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,14 @@
 
 date_default_timezone_set('UTC');
 
+// Set our constants to the global env for travis if env exists
+if (getenv('TWITCH_TEST_CLIENT_ID') !== false) {
+    define('TWITCH_TEST_CLIENT_ID', getenv('TWITCH_TEST_CLIENT_ID'));
+}
+
+if (getenv('TWITCH_TEST_CLIENT_SECRET') !== false) {
+    define('TWITCH_TEST_CLIENT_SECRET', getenv('TWITCH_TEST_CLIENT_SECRET'));
+}
+
 // Run the composer autoload
 require_once(__DIR__ . '/../vendor/autoload.php');
